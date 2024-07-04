5f61b0414e59a79d312c02aea37631d10a6844f0
Mujahid Islam <mbiplobe@gmail.com>
2024-07-04 13:30:05 +0600

    Initial Commit

---
+++ CountrySwiftProject/CountrySwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountrySwiftProjectApp.swift
+//  CountrySwiftProject
+//
+//  Created by Mujahid Islam on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CountrySwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
